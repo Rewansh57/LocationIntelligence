4a8d17145fe096f35f1f16ffbd0fd4320fdab578
rewansh choudhary <rewanshchoudhary57@gmail.com>
2025-06-29 00:28:10 +0530

    v1

---
+++ src/main/java/com/example/Location/Intelligence/LocationIntelligence.java
@@ -0,0 +1,13 @@
+package com.example.Location.Intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationIntelligence {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationIntelligence.class, args);
+	}
+
+}
