c9057b52446d4fa59ef7d2acc419271028211511
rewansh choudhary <rewanshchoudhary57@gmail.com>
2025-06-29 15:34:03 +0530

    v1

--- src/main/java/com/example/Location/Intelligence/LocationIntelligence.java
+++ src/main/java/com/example/Location/Intelligence/LocationIntelligence.java
@@ -1,13 +1,20 @@
 package com.example.Location.Intelligence;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocationIntelligence {
+public class LocationIntelligence implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocationIntelligence.class, args);
+
+
+	}
+	public void run (String... args) throws Exception {
+		System.out.println("LocationIntelligence");
+
 	}
 
 }
